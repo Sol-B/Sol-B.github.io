d76b4bbec5d748a349d2f06d4ec36f00991fbc27
ladybirdbox <shamobigxiyi@qq.com>
2023-04-11 23:12:24 +0800

    Site updated: 2023-04-11 23:12:23

--- js/article-img.js
+++ js/article-img.js
@@ -1,12 +1,12 @@
-window.addEventListener('load',function(){  
+// window.addEventListener('load',function(){  
 
-    let img = document.querySelectorAll('.jsimg');
+//     let img = document.querySelectorAll('.jsimg');
 
-    img.forEach(function(value) {
-        let marginLeft = value.clientWidth;
-        let marginLeft_ture = (900-marginLeft)/2;
-        marginLeft_ture = marginLeft_ture.toFixed(1);
-        value.style.marginLeft = marginLeft_ture+'px';
-    });
+//     img.forEach(function(value) {
+//         let marginLeft = value.clientWidth;
+//         let marginLeft_ture = (900-marginLeft)/2;
+//         marginLeft_ture = marginLeft_ture.toFixed(1);
+//         value.style.marginLeft = marginLeft_ture+'px';
+//     });
 
-});
+// });
