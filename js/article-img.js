ee92dcf16a5e694b3de78cde4844db23db860479
ladybirdbox <shamobigxiyi@qq.com>
2023-04-12 01:18:19 +0800

    Site updated: 2023-04-12 01:18:18

--- js/article-img.js
+++ js/article-img.js
@@ -1,12 +1,35 @@
-// window.addEventListener('load',function(){  
+const body = document.querySelector("body");
+// 文章中所有的图片
+const articleImg = document.querySelectorAll("#js_articleImg");
+// 图片背景
+const BackgroundBoard = document.querySelector("#js_BackgroundBoard");
+// 关闭按钮
+const closeButton = document.querySelector("#js_closeButton");
 
-//     let img = document.querySelectorAll('.jsimg');
 
-//     img.forEach(function(value) {
-//         let marginLeft = value.clientWidth;
-//         let marginLeft_ture = (900-marginLeft)/2;
-//         marginLeft_ture = marginLeft_ture.toFixed(1);
-//         value.style.marginLeft = marginLeft_ture+'px';
-//     });
+// 图片点击监听函数
+articleImg.forEach((value, index)=>{
+    value.addEventListener('click', ()=>{
 
-// });
+        // 显示黑色背景
+        BackgroundBoard.setAttribute('style', 'display:block; overflow:hidden;');
+        // 显示关闭图标
+        closeButton.setAttribute('style', 'display: block;');
+
+        // 注入点击的图片
+        BackgroundBoard.innerHTML = value.innerHTML;
+        // 防止图片打开后有滚动条
+        body.setAttribute('style', 'overflow: hidden;');
+
+    } );
+});
+
+
+// 关闭按钮监听函数
+closeButton.addEventListener('click', ()=>{
+    // 关闭并隐藏
+    BackgroundBoard.setAttribute('style', 'display: none;');
+    closeButton.setAttribute('style', 'display: none;');
+    // 图片关闭后打开滚动条
+    body.setAttribute('style', 'overflow: ');
+});
